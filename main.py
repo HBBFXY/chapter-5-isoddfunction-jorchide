44a1bcdf6d9ca452e07a767ba69ede2ace583155
李佳赛 <2215317512@qq.com>
2025-10-24 10:14:36 +0800

    更新 main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def isOdd(value):
-    if isinstance(value,int) and not isinstance(value,bool):
-        return value%2!=0
-    else:
-        return false
+def isOdd(n):
+    # 先判断是否是整数（排除bool类型，因为bool是int的子类）
+    if isinstance(n, int) and not isinstance(n, bool):
+        # 是整数，再判断是否为奇数
+        return n % 2 != 0
+    # 非整数或其他情况，返回False
+    return False
